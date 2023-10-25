f051b164fe0091a5f10aa624ee68c3c020975583
Kaare <kaare.steensig@gmail.com>
2023-10-25 17:05:49 +0200

    clean up

--- build/capture_image/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/bach/DWARVES/src/capture_image
-# Build directory: /home/ros/bach/DWARVES/build/capture_image
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
