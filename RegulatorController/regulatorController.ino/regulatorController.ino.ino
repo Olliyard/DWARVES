9caaf59a5f555a11ada870724a5e53bc4e8c2c9d
Olliyard <oliver.malling@upteko.com>
2023-12-05 18:45:33 +0100

    added regulatorController main with outlines for every feature

--- RegulatorController/regulatorController.ino/regulatorController.ino.ino
+++ RegulatorController/regulatorController.ino/regulatorController.ino.ino
@@ -10,7 +10,9 @@
 #define PIN_OUTPUT 9
 #define ONE_WIRE_BUS A0
 const byte numChars = 32;
-const byte maxColonies = 3; 
+const int numColonies = 3;
+const int numChannels = 3;
+
 char receivedChars[numChars];
 char tempChars[numChars];        // temporary array for use when parsing
 
@@ -21,7 +23,7 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 // Variables
-int dutyCycle[3] = {0, 0, 0}; // 0 - red, 1 - blue, 2 - temp
+int dutyCycle[numColonies] = {0}; // 0 - red, 1 - blue, 2 - temp
 int deviceCount = 0;
 float tempC;
 boolean newData = false;
@@ -52,18 +54,18 @@ void setup() {
   sensors.begin();
 
   // Create colony struct array with maxColonies indexes. (colony1 = index 0 etc)
-  struct Colony colonies[maxColonies];
+  struct Colony colonies[numColonies];
 }
 
+
 void loop() {
-  int currColony = 0;
-  recvData();
-  if (newData == true){
-    strcpy(tempChars, receivedChars); // this temporary copy is necessary to protect the original data
-    currColony = parseData();
-    // Go through PWM and set it for each channel.
-    setPWM(currColony, colonies[currColony].redBrightness, colonies[currColony].blueBrightness, colonies[currColony].setTemp);
-    newData = false;
+  int currColony = 0;                 // current colony handled
+  recvData();                         // read the data
+  if (newData == true){               // if newData flag has been set
+    strcpy(tempChars, receivedChars); // make copy of original data
+    currColony = parseData();         // parse the data and save the current colony
+    setPWM(currColony, colonies[currColony].redBrightness, colonies[currColony].blueBrightness, colonies[currColony].setTemp);  // setPWM with the current colony struct settings.
+    newData = false;                  // reset newData flag
   }
   delay(500); // Adjust the delay as needed
 }
@@ -73,14 +75,17 @@ void setPWM(int colonyID, int red, int blue, int temp) {
   mapDutyCycle(red, blue, temp);
   digitalWrite(PIN_INH, LOW); // ON
   /* Would love to simplify lines 88 - 148 by using something similar to the below.
-  for (i=0; i<=maxColonies*3; i++){
-    if (colonyID == 1) {
-      digitalWrite(PIN_A, i); // 
-      digitalWrite(PIN_B, i);
-      digitalWrite(PIN_C, i);
-      analogWrite(PIN_OUTPUT, dutyCycle[0]);
+  for (colonyID=1; colonyID<=numColonies; colonyID++){
+    digitalWrite(PIN_INH, LOW);
 
+    for (int channel = 0; channel < numChannels; channel++) {
+      digitalWrite(PIN_A, (channel & 0b001) ? HIGH : LOW);  // Set PIN_A based on the lowest bit of channel
+      digitalWrite(PIN_B, (channel & 0b010) ? HIGH : LOW);  // Set PIN_B based on the second lowest bit of channel
+      digitalWrite(PIN_C, (channel & 0b100) ? HIGH : LOW);  // Set PIN_C based on the third lowest bit of channel
+      analogWrite(PIN_OUTPUT, dutyCycle[channel]);          // Set the duty cycle for the corresponding channel
+      delay(100);
     }
+    digitalWrite(PIN_INH, HIGH);
   }
   */
 
